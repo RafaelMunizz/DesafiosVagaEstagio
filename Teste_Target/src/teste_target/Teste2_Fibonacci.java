2e45038ee1f3c633e3608022309cfeab26eb8b68
Rafael Muniz <58229800+RafaelMunizz@users.noreply.github.com>
2022-06-17 20:51:24 -0300

    Create Teste2_Fibonacci.java

---
+++ Teste_Target/src/teste_target/Teste2_Fibonacci.java
@@ -0,0 +1,45 @@
+package teste_target;
+
+/**
+ * @author Rafael_Muniz
+ */
+
+public class Teste2_Fibonacci {
+
+    public static void main(String[] args) {
+        
+        System.out.println(pertenceFibonacci(48));
+        System.out.println(pertenceFibonacci(15));
+        System.out.println(pertenceFibonacci(34));
+        System.out.println(pertenceFibonacci(50));
+    }
+    
+    private static int fibonacci(int num){
+        
+        switch (num) {
+            case 0:
+                return 0;
+            case 1:
+                return 1;
+            default:
+                return fibonacci(num - 1) + fibonacci(num - 2);
+        }
+    }
+    
+    public static boolean pertenceFibonacci(int num){
+        
+        int i = 0;
+        while(true){
+            
+            int var = fibonacci(i);
+            
+            if (var == num){
+                return true;
+            } else if (var > num){
+                break;
+            }
+            i++;
+        }
+        return false;
+    }
+}
